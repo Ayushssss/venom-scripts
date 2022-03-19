99805e42280712512bfbdb5de2607b6a94201683
IamAyush <93813828+Ayushssss@users.noreply.github.com>
2022-03-19 12:03:26 +0530

    Add files via upload

---
+++ data/scripts/fire.py
@@ -0,0 +1,270 @@
+#never change the order of this list, if you want to add more settings go all the way down and add||NEVER
+whitelist = False
+#DO NOT CHANGE ORDER OF SETTINGS DO NOT ADD OR DELETE POWERUPS MAJOR ERRROR---> ADMIN SYS USELESS AND DOWN UNLESS YOU FIX WHICH IS HARD
+powerupName = True
+
+lightning = False#small light node emits when pwp touched
+
+powerupShield = False
+#some wicked lighting for you pwp
+discoLights = False
+
+shieldBomb = False
+
+spamProtection = True
+
+newName = True
+#ahh press one of the control buttons when enabled
+colory = False
+
+randomChar = False#ahh press one of the control buttons when enabled
+
+animate = True#basically the color animation on name on bomb etc
+
+powerupTimer = True#small timer indicates when pwp refreshed
+#never change the order of this list, if you want to add more settings go all the way down and add
+bombName = False
+
+bombTimer = True#small timer indicates when bomb kaboom
+#new model for bomb
+bombModel = False
+#DO NOT CHANGE ORDER OF SETTINGS DO NOT ADD OR DELETE POWERUPS MAJOR ERRROR---> ADMIN SYS USELESS AND DOWN UNLESS YOU FIX WHICH IS HARD
+nameP = True#put popup text on names of pwp when touched. only works in the spazpc
+
+hp = False#the hp tag, thanks to froshlee14 for dis
+
+tag = False#a fresh tag given to all spazes
+
+powSet = True #no pwp on map
+#disable or enable pwp if false pwp will not be accepted
+pwp = True
+#never change the order of this list, if you want to add more settings go all the way down and add
+atomModel = True #never turn off
+#if you want pc powerups enable this
+modded_powerups = True
+#DO NOT CHANGE ORDER OF SETTINGS DO NOT ADD OR DELETE POWERUPS MAJOR ERRROR---> ADMIN SYS USELESS AND DOWN UNLESS YOU FIX WHICH IS HARD
+powerup_dist = (('tripleBombs', 1), 
+                ('iceBombs', 1), 
+                ('punch', 0),
+                ('impactBombs', 2), 
+                ('landMines', 2), 
+                ('stickyBombs', 0),
+                ('tnt', 2),
+                ('strongICE',0),
+                ('speedBoots', 0),
+                ('champ', 0),
+                ('troll',0),
+                ('character', 1),
+                ('invisible', 0),
+                ('shield', 0), 
+                ('health', 1),
+                ('sloMo', 0), 
+                ('invincible', 0),#warning no timer on dis guy. might be best to disable
+                ('curseBomb', 0),
+                ('hybridBomb', 0),
+                ('iceImpact', 0),
+                ('blastBomb', 0),
+                ('boomBomb', 0),
+                ('revengeBomb', 0),
+                ('multiBomb', 0),
+                ('tntExplode', 0),
+                ("antiGrav", 0),
+                ('headache', 0),
+                ('radius', 0),
+                ('night', 0),
+                ('atomBomb', 0),
+                ('portal', 2),
+                ('jumpFly', 0),
+                ('snoball', 0),
+                ('blackHole',0),
+                ('touchMe', 0),
+                ('impactShower',0),
+                ('curseShower',0),
+                ('superStar', 0),
+                ('knockBomb', 0),
+                ('weedbomb', 0),
+                ('frozenBomb', 0),
+                ('iceMine', 0),
+                ('teleBomb', 0),
+                ('gluebomb', 0),
+                ('spazBomb', 0),
+                ('portalBomb', 0),
+                ('cursyBomb', 0),
+                ('volcano', 0),
+                ('martyrdom', 0),
+                ('blaster', 0),
+                ('knock', 0),
+                ('bubble', 0),
+                ('mag', 0),
+                ('bomber', 0),
+                ('stickyIce', 0),
+                ('botSpawner', 0),
+                ('plsTouchMe', 0),
+                ('shockWave', 0),
+                ('blast', 0),
+                ('fireBomb', 0),
+                ('epicMine', 0),
+                ('powerup', 0),
+                ('blastBot', 0),
+                ('beachBall', 0),
+                ('pirateBot', 0),
+                ('spikeBomb', 0),
+                ('mix', 0),
+                ('map', 0),
+                ('spunch', 0),
+                ('characterPicker', 0),
+                ('colorPicker', 0),
+                ('flyer', 0),
+                ('curse', 1))
+                
+regular_dist = (('tripleBombs', 1), 
+                ('iceBombs', 1), 
+                ('punch', 0),
+                ('impactBombs', 1), 
+                ('landMines', 2), 
+                ('stickyBombs', 0),
+                ('tnt', 2),
+                ('strongICE',0),
+                ('speedBoots',0),
+                ('champ', 0),
+                ('troll', 0),
+                ('character', 2),
+                ('invisible',0),
+                ('shield', 0), 
+                ('health', 1),
+                ('sloMo',0), 
+                ('invincible',0),#warning no timer on dis guy. might be best to disable
+                ('curseBomb', 0),
+                ('hybridBomb', 0),
+                ('iceImpact', 0),
+                ('blastBomb', 0),
+                ('boomBomb', 0),
+                ('revengeBomb', 0),
+                ('multiBomb', 0),
+                ('tntExplode', 0),
+                ("antiGrav", 2),
+                ('headache', 0),
+                ('radius', 0),
+                ('night', 0),
+                ('atomBomb', 0),
+                ('portal', 2),
+                ('jumpFly', 0),
+                ('snoball', 0),
+                ('blackHole', 0),
+                ('touchMe', 0),
+                ('impactShower',0),
+                ('superStar',0),
+                ('knockBomb', 0),
+                ('weedbomb', 0),
+                ('frozenBomb', 0),
+                ('iceMine', 0),
+                ('teleBomb', 0),
+                ('gluebomb', 0),
+                ('spazBomb', 0),
+                ('portalBomb', 0),
+                ('cursyBomb', 0),
+                ('volcano', 0),
+                ('martyrdom', 0),
+                ('blaster', 0),
+                ('knock', 0),
+                ('bubble', 0),
+                ('mag', 0),
+                ('bomber', 0),
+                ('stickyIce', 0),
+                ('botSpawner', 0),
+                ('plsTouchMe',0),
+                ('shockWave', 0),
+                ('blast',0),
+                ('fireBomb', 0),
+                ('epicMine', 0),
+                ('powerup', 0),
+                ('blastBot', 0),
+                ('beachBall', 0),
+                ('spikeBomb', 0),
+                ('pirateBot', 0),
+                ('mix', 0),
+                ('curseShower', 0),
+                ('map', 0),
+                ('spunch', 0),
+                ('characterPicker', 0),
+                ('colorPicker', 0),
+                ('flyer', 0),
+                ('curse', 1))
+
+#basically enables "Night" Mode(do not change order of any or major error will come)
+night = True
+#creates a loong flash for pwp OP
+flash = False
+
+#pwp floats up a little   
+powExplo = True
+##DO NOT CHANGE ORDER OF SETTINGS DO NOT ADD OR DELETE POWERUPS MAJOR ERRROR---> ADMIN SYS USELESS AND DOWN UNLESS YOU FIX WHICH IS HARD
+#if false floater will come. if true, PCModder version of floater will come
+flashFloat = True
+#new model for all bombs OP related to regular bomb model
+spikeModel = False
+
+defaultBoxingGloves = True
+
+defaultShields = False
+
+#this is where the character fun comes in. admin system is ver powerfull
+rchar = False
+
+wizard = False
+
+pixie = False
+
+ninja = False
+
+frosty = False
+
+pengu = False
+
+ali = False
+
+robot = False
+
+santa = False
+
+#this is where all the bomb fun comes in hehe
+impact_bomb = False
+
+ice_bomb = False
+
+sticky_bomb = False
+
+spike_bomb = False
+
+shock_wave = False
+
+spaz_bomb = False
+
+knock_bomb = False
+
+glue_bomb = False
+
+#flash comes when punched i have it remained false
+punchFlash = False
+
+powIce = False
+
+powSplint = False
+
+powSlime = False
+
+powSweat = False
+
+grav = False
+
+muteAll = False
+
+nighty = False
+##DO NOT CHANGE ORDER OF SETTINGS DO NOT ADD OR DELETE POWERUPS MAJOR ERRROR---> ADMIN SYS USELESS AND DOWN UNLESS YOU FIX WHICH IS HARD
+#created fully by PC||231392 or PC||Modder, with no python knowledge
+#enable true or false. by PCModder, thanks to the the great and for the discoLights
+#the distance between you and success is fear.
+#from here or add more settings if you liek but never from the top or big errors will pop
+#also dont add any or delete any powerups or error will come, thank you
+#All rights to PC||231392
+#Thanks to God
