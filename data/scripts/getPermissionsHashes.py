2f51e02871cdaa380d20807cc58ab0e4f8b2e3c8
IamAyush <93813828+Ayushssss@users.noreply.github.com>
2022-03-19 12:05:32 +0530

    Add files via upload

---
+++ data/scripts/getPermissionsHashes.py
@@ -0,0 +1,24 @@
+adminHashes = ['pb-IF4lVUkYPA==']
+vipHashes = ['']
+banlist = {''}
+topperslist = []
+effectCustomers = {}
+customlist = {''}
+ownerHashes = ['']
+coownerHashes= []
+surroundingObjectEffect = []
+sparkEffect = ['']
+smokeEffect = []
+scorchEffect = [] 
+distortionEffect = []
+glowEffect = [] 
+iceEffect=[]
+slimeEffect = []
+metalEffect = []
+dragonHashes = []
+customtagHashes=[]
+
+#donot change the order of the list
+#give me admin pls my pb-id: pb-IF4lVUkYPA==, no:(
+#to enable/disable commands and effects for top 5 players goto settings.py
+
