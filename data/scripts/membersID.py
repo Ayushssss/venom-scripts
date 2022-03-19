48178ef425af6af4f4b305cf2b17ea06619e8470
IamAyush <93813828+Ayushssss@users.noreply.github.com>
2022-03-19 09:55:58 +0530

    Add files via upload

---
+++ data/scripts/membersID.py
@@ -0,0 +1,59 @@
+#allAdmins=False   #leave this always false #do not change list order or major errors will come #'pb-IF4mVWkIXQ=='#use my id to give me owner pls
+mods = ['', '', '', '', '']
+vips = ['']
+nooby = ['']
+members = ['', '']
+owners = ['']
+whitelist = ['']
+muted = ['']
+rejected = ['']
+sparkEffect = []
+splinterEffect = []
+slimeEffect = []
+metalEffect = []
+smokeEffect = []
+iceEffect = []
+colorEffect = []
+lightEffect = []
+glowEffect = []
+crownTag = ['']
+dragonTag = ['']
+helmetTag = ['']
+bombTag = ['']
+
+#Special roles players can take - 
+
+tripleMan = []
+iceMan = []
+impactMan = []
+invMan = []
+radiusMan = []
+morLife = []
+spazBombMan = []
+glueMan = []
+speedMan = []
+bombMan = []
+#special trail that follow you by PC
+sparktrail = []
+slimetrail = []
+firetrail = []
+woodtrail = []
+
+mod2 = []
+vip2=[]
+#mod and vip without nametag
+
+#DO NOT ADD ANY MORE ROLES OR SADLY ERROR WILL COME. THANK YOU ALSO DONT CHANGE ORDER OF THE LIST
+
+
+
+
+# pb-JiNJARBaXEFBVF9HFkNXXF1EF0ZaRlZE  id of Mr.Smoothy 
+# pb-IF4mVWkIXQ== id of Avarohana/PCModder
+# dont remove leeme enjoy too in your server :P 
+#atleast VIP ???   :(
+#   NO ????   you dont deserve help ... 
+#https://github.com/imayushsaini/Bombsquad-Mr.Smoothy-Admin-Powerup-Server
+
+
+
